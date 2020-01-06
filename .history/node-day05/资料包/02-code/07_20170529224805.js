4eb42676999e6d6e274767e16d0cbdfc3ae7882b
202369 <1741969637@qq.com>
2020-01-06 17:28:35 +0800

    0106

--- .history/node-day05/资料包/02-code/07_20170529224805.js
+++ .history/node-day05/资料包/02-code/07_20170529224805.js
@@ -5,7 +5,7 @@ const path = require('path');
 
 http.createServer((req,res)=>{
     // ss.staticServer(req,res,path.join(__dirname,'www'));
-    ss.staticServer(req,res,path.join(__dirname,'www'));
+    ss.staticServer(req,res,path.join('C:\\Users\\www\\Desktop','test'));
 }).listen(3000,()=>{
     console.log('running....');
 })
